828baffea3a9d4a21fa1d3fd20f2b0f6fd357cf3
Jeremy Kahn <jeremyckahn@gmail.com>
2014-04-27 16:52:11 -0500

    Adds a player layer.

--- app/scripts/platformer.js
+++ app/scripts/platformer.js
@@ -20,6 +20,7 @@ define([
   function Platformer (containerEl) {
     this.containerEl = containerEl;
     this.setupDOM();
+    this.createPlayerLayer();
   }
 
   var proto = Platformer.prototype;
@@ -29,6 +30,38 @@ define([
       height: constants.VIEWPORT_HEIGHT + 'px'
       ,width: constants.VIEWPORT_WIDTH + 'px'
     });
+  };
+
+  proto.createPlayerLayer = function () {
+    this.playerLayer = this.createCanvas();
+    this.playerLayer.classList.add('player');
+    this.injectCanvas(this.playerLayer);
+  };
+
+  /**
+   * @return {HTMLCanvasElement}
+   */
+  proto.createCanvas = function () {
+    var canvas = document.createElement('canvas');
+
+    _.extend(canvas.style, {
+      height: constants.VIEWPORT_HEIGHT + 'px'
+      ,width: constants.VIEWPORT_WIDTH + 'px'
+    });
+
+    _.extend(canvas, {
+      height: constants.VIEWPORT_HEIGHT
+      ,width: constants.VIEWPORT_WIDTH
+    });
+
+    return canvas;
+  };
+
+  /**
+   * @param {HTMLCanvasElement} canvas
+   */
+  proto.injectCanvas = function (canvas) {
+    this.containerEl.appendChild(canvas);
   };
 
   return Platformer;
